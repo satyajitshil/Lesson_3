137a688b7bd7cc6b7ef54ecd529cbe4f488b8078
Satyajit Shil <satyajitshil02@gmail.com>
2025-05-23 16:50:17 -0700

    Indexing

---
+++ str_ops.py
@@ -0,0 +1,9 @@
+str1 = "Codingal"
+print(str1[0:6:1])
+print(str1[::-1])
+print(str1[::3])
+str2 = "Welcome to "
+print("str2 + str1 = ", str2 + str1)
+print(str1[0::1])
+text = str(input("Type a string: "))
+print(text[::-1])
